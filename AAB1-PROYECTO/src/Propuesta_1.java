cccf86bc6b3bfcda8e83aee79fe4088991d0375f
JU4NSCV <jscueva5@utpl.edu.ec>
2023-12-03 09:56:59 -0500

    Propuesta_1

--- AAB1-PROYECTO/src/Propuesta_1.java
+++ AAB1-PROYECTO/src/Propuesta_1.java
@@ -39,5 +39,52 @@ public class Propuesta_1 {
         System.out.println("______________________________________________");
         System.out.println("The percentage of students approved is: "+percentApp+"%");
         System.out.println("The percentage of students not approved is: "+percentRep+"%");
+        /*
+Enter the name of the Student: a
+Enter the ID Student: 1
+--------//REPORT//--------
+--NOTES FOR BIMONTHLY--
+Note ACD:  5
+Note APE:  10
+Note AA:  8
+Total Note:  7.666666666666667
+Student is approved for next semester.
+______________________________________
+Do you want to enter one more student?s
+Enter the name of the Student: b
+Enter the ID Student: 2
+--------//REPORT//--------
+--NOTES FOR BIMONTHLY--
+Note ACD:  8
+Note APE:  8
+Note AA:  9
+Total Note:  8.333333333333334
+Student is approved for next semester.
+______________________________________
+Do you want to enter one more student?s
+Enter the name of the Student: c
+Enter the ID Student: 3
+--------//REPORT//--------
+--NOTES FOR BIMONTHLY--
+Note ACD:  1
+Note APE:  8
+Note AA:  3
+Total Note:  4.0
+Student is not approved for next semester.
+______________________________________
+Do you want to enter one more student?s
+Enter the ID Student: 4
+--------//REPORT//--------
+--NOTES FOR BIMONTHLY--
+Note ACD:  9
+Note APE:  9
+Note AA:  2
+Total Note:  6.666666666666667
+Student is not approved for next semester.
+Do you want to enter one more student?n
+______________________________________________
+The percentage of students approved is:  50.0 %
+The percentage of students not approved is:  50.0 %
+         */
     }
 }
